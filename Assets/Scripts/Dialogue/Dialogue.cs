ec41c2ad617dda462f6fdec9f1e1617794d6da9f
impoimpoimpo <aimpo@outlook.com>
2020-06-08 12:18:51 +0930

    Refactored dialogue creation tests

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -8,4 +8,14 @@ public class Dialogue
     public string name;
     public string sentence;
     public Sprite image;
+
+    public static Dialogue Create (string name, string sentence, Sprite image = null)
+    {
+        return new Dialogue
+        {
+            name = name,
+            sentence = sentence,
+            image = image
+        };
+    }
 }
--- Assets/Scripts/Dialogue/DialogueInitializer.cs
+++ Assets/Scripts/Dialogue/DialogueInitializer.cs
@@ -18,40 +18,15 @@ public class DialogueInitializer : MonoBehaviour
     {
         dialogues = new Dictionary<string, Dialogue[]>();
         Dialogue[] dialogue = new Dialogue[3];
-        dialogue[0] = new Dialogue {
-            name = "Jeff",
-            sentence = "Hey!"
-        };
-        dialogue[1] = new Dialogue
-        {
-            name = "Jeff",
-            sentence = "You need to show me your ID."
-        };
-        dialogue[2] = new Dialogue
-        {
-            name = "Jeff",
-            sentence = "Please come here and show me your ID."
-        };
-
+        dialogue[0] = Dialogue.Create("Jeff", "Hey!");
+        dialogue[1] = Dialogue.Create("Jeff", "You need to show me your ID.");
+        dialogue[2] = Dialogue.Create("Jeff", "Please come here and show me your ID.");
         dialogues.Add("TEST", dialogue);
 
         Dialogue[] dialogue2 = new Dialogue[3];
-        dialogue2[0] = new Dialogue
-        {
-            name = "Jeff",
-            sentence = "Show me your ID!"
-        };
-        dialogue2[1] = new Dialogue
-        {
-            name = "Jeff",
-            sentence = "Use the inventory selector on the right of our conversation tab."
-        };
-        dialogue2[2] = new Dialogue
-        {
-            name = "Jeff",
-            sentence = "And select your ID to show me."
-        };
-
+        dialogue2[0] = Dialogue.Create("Jeff", "Show me your ID!");
+        dialogue2[1] = Dialogue.Create("Jeff", "Use the inventory selector on the right of our conversation tab.");
+        dialogue2[2] = Dialogue.Create("Jeff", "And select your ID to show me.");
         dialogues.Add(DialogueKeys.SHOW_ID_TO_GUARD, dialogue2);
     }
 
